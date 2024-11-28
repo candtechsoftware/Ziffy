74eb61567b7a80601c7cc616942950c9a60c7427
Alex Candelario <alex@candtechsoftware.com>
2024-11-27 22:00:59 -0500

    Initial commit with the header parsed

---
+++ build.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const opts = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "ws_server",
+        .root_source_file =  b.path("src/main.zig"),
+        .optimize = opts,
+        .target =  target,
+    });
+    std.debug.print("build {any}", .{b.args});
+
+    b.installArtifact(exe);
+}
