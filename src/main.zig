dcf27e394ca578141a84d8d107a94e2aa74080d0
candtechsoftware <alex@candtechsoftware.com>
2024-12-01 00:59:48 -0500

    Still adding logic around parsing application info and Frames

--- src/main.zig
+++ src/main.zig
@@ -71,6 +71,70 @@ const ExtKind = enum(u8) {
     app_ext = 0xff,
 };
 
+const ApplicationExtSignature = extern struct {
+    ident: [8]u8,
+    auth_code: [3]u8,
+};
+
+const ApplicationExt = struct {
+    sig: ApplicationExtSignature,
+    data: []u8,
+
+    pub fn deinit(self: *ApplicationExt, allocator: std.mem.Allocator) void {
+        allocator.free(self.data);
+    }
+};
+
+const DisposalMethod = enum(u3) {
+    none = 0,
+    do_not_dispose = 1,
+    restore_background_color = 2,
+    restore_previous = 3,
+    _,
+};
+const GraphicControlExtFlags = packed struct(u8) {
+    has_transparent_color: bool = false,
+    user_input: bool = false,
+    disposal_method: DisposalMethod = .none,
+    reversed: u3 = 0,
+};
+
+const GraphicControlExt = extern struct {
+    flags: GraphicControlExtFlags align(1) = .{},
+    delay: u16 align(1) = 0,
+    transparent_index: u8 align(1) = 0,
+};
+
+const ImageDescriptorFlags = packed struct(u8) {
+    local_color_table_size: u3 = 0,
+    has_local_color_table: bool = false,
+    is_interlaced: bool = false,
+    reseverd: u2 = 0,
+    sort: bool = false,
+};
+
+const Comment = struct {
+    comment: []u8,
+
+    pub fn denit(self: *Comment, allocator: std.mem.Allocator) void {
+        allocator.free(self.comment);
+    }
+};
+
+const ImageDescriptor = struct {
+    left_pos: u16 align(1) = 0,
+    top_pos: u16 align(1) = 0,
+    width: u16 align(1) = 0,
+    height: u16 align(1) = 0,
+    flags: ImageDescriptorFlags align(1) = .{},
+};
+const SubBlock = struct {
+    local_color_table: FixedArray(Rgb24, 256) = .{},
+    image_descriptor: ImageDescriptor = .{},
+    pixels: []u8 = &.{},
+};
+const Frame = struct { graphics_control: ?GraphicControlExt = null, sub_block: std.ArrayListUnmanaged(SubBlock) = .{} };
+
 const GifParser = struct {
     header: Header = .{},
     logical_descriptor: LogicalDescriptor = .{},
@@ -78,6 +142,10 @@ const GifParser = struct {
     global_color_table: FixedArray(Rgb24, 256) = .{},
     data: []const u8,
     cursor: usize,
+    current_frame: ?*Frame = null,
+    has_anitmation_app_ext: bool = false,
+    frames: std.ArrayListUnmanaged(Frame) = .{},
+    comments: std.ArrayListUnmanaged(Comment) = .{},
 
     pub fn init(allocator: std.mem.Allocator, data: []const u8) GifParser {
         return .{
@@ -92,7 +160,13 @@ const GifParser = struct {
         _ = self;
     }
 
-    pub fn parse(self: *GifParser) ParserError!void {
+    fn alloc_frame(self: *GifParser) !*Frame {
+        const frame = try self.frames.addOne(self.allocator);
+        frame.* = Frame{};
+        return frame;
+    }
+
+    pub fn parse(self: *GifParser) anyerror!void {
         self.cursor = @sizeOf(Header);
         self.header = std.mem.bytesToValue(Header, self.data[0..self.cursor]);
         if (!std.mem.eql(u8, self.header.tag[0..], TAG)) {
@@ -129,7 +203,7 @@ const GifParser = struct {
         try self.read_data();
     }
 
-    fn read_data(self: *GifParser) !void {
+    fn read_data(self: *GifParser) anyerror!void {
         var current_block = try byteToEnum(BlockKind, self.data[self.cursor]);
         self.cursor += 1;
         while (current_block != .eof) {
@@ -170,12 +244,60 @@ const GifParser = struct {
                     break;
                 },
             }
+            std.debug.print("after all {any}\n", .{ext_kind});
             if (is_graphic_block) {
-                std.debug.print("G{any}\n", .{current_block});
+                std.debug.print("Graphics Block! {any}\n", .{current_block});
                 try self.read_graphics_block(current_block, ext_kind);
             } else {
-                std.debug.print("Not G{any}\n", .{current_block});
+                std.debug.print("Not Graphics Block {any}\n", .{current_block});
+                try self.read_non_graphics_block(ext_kind.?);
             }
+        }
+    }
+
+    fn read_non_graphics_block(self: *GifParser, ext_kind: ExtKind) !void {
+        std.debug.print("Non g func {any} \n", .{ext_kind});
+        switch (ext_kind) {
+            .comment => {
+                var new_comment = try self.comments.addOne(self.allocator);
+                var comment_list = try std.ArrayListUnmanaged(u8).initCapacity(self.allocator, 256);
+                defer comment_list.deinit(self.allocator);
+
+                var block_size = self.data[self.cursor];
+                self.cursor += 1;
+
+                while (block_size > 0) {
+                    var block: []const u8 = undefined;
+                    block = self.data[self.cursor .. self.cursor + block_size];
+                    self.cursor += block_size;
+                    try comment_list.appendSlice(self.allocator, block[0..]);
+                    block_size = self.data[self.cursor];
+                    self.cursor += 1;
+                }
+                new_comment.comment = try self.allocator.dupe(u8, comment_list.items);
+                std.debug.print("Comment {s}", .{new_comment.comment});
+            },
+            .app_ext => {
+                const new_app_info = blk: {
+                    var app_info: ApplicationExt = undefined;
+                    var app_sig: ApplicationExtSignature = undefined;
+                    self.cursor += 1;
+
+                    const sig_size = @sizeOf(ApplicationExtSignature);
+                    app_sig = std.mem.bytesToValue(ApplicationExtSignature, self.data[self.cursor..self.cursor + sig_size]);
+                    self.cursor += sig_size;
+
+                    app_info.sig = app_sig;
+
+                    std.debug.print("App info: id: {s} :: auth: {s}\n", .{app_info.sig.ident, app_info.sig.auth_code});
+
+                    break :blk app_info;
+                };
+                _ = new_app_info;
+            },
+            else => {
+                std.debug.print("EXT kind:: {any}", .{ext_kind});
+            },
         }
     }
 
@@ -183,6 +305,22 @@ const GifParser = struct {
         if (ext_kind) |kind| {
             if (kind == .graphic_control) {
                 std.debug.print("HERE:: {any} ::  {any} \n", .{ self.data[self.cursor..(self.cursor + 100)], block_kind });
+                self.current_frame = try self.alloc_frame();
+                std.debug.print("out!Q ", .{});
+                self.current_frame.?.graphics_control = blk: {
+                    std.debug.print("in:: ", .{});
+                    var control: GraphicControlExt = undefined;
+                    // eat block_size
+                    self.cursor += 1;
+
+                    control.flags = std.mem.bytesToValue(GraphicControlExtFlags, self.data[self.cursor..@sizeOf(GraphicControlExtFlags)]);
+                    self.cursor += @sizeOf(GraphicControlExtFlags);
+                    control.delay = std.mem.bytesToValue(u16, self.data[self.cursor .. self.cursor + 1]);
+                    self.cursor += 2;
+                    std.debug.print("control: {any}", .{control});
+
+                    break :blk control;
+                };
             }
         }
     }
